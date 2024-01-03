85b707b99b22ab6b79c5cbb6f2c4f0a9e0132d9c
evtepo <andrey_laguta00@mail.ru>
2024-01-04 00:20:58 +1000

    Код для подготовки данных

---
+++ etl/transformer.py
@@ -0,0 +1,39 @@
+from typing import List, Dict, Any, Union
+
+
+def get_valid_data(
+    id: str,
+    rating: float,
+    genre: List[str],
+    title: str,
+    description: str,
+    persons: List[Union[Dict[str, Any], None]],
+) -> Dict[str, Any]:
+    """Функция для преобразования данных."""
+
+    people = {
+        "writer": [],
+        "actor": [],
+        "director": [],
+    }
+
+    for person in persons:
+        people.setdefault(person.get("person_role"), []).append(
+            {
+                "id": person.get("person_id"),
+                "name": person.get("person_name"),
+            }
+        )
+
+    return {
+        "id": id,
+        "imdb_rating": rating,
+        "genre": list(genre),
+        "title": title,
+        "description": description,
+        "director": list(map(lambda x: x.get("name"), people.get("director"))),
+        "actors_names": list(map(lambda x: x.get("name"), people.get("actor"))),
+        "writers_names": list(map(lambda x: x.get("name"), people.get("writer"))),
+        "actors": list(people.get("actor")),
+        "writers": list(people.get("writer")),
+    }

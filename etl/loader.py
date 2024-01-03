4136bcf09409de9415090c788932f06fdcb398f5
evtepo <andrey_laguta00@mail.ru>
2024-01-04 00:18:28 +1000

    Код для загрузки данных в Elasticsearch

---
+++ etl/loader.py
@@ -0,0 +1,22 @@
+from backoff import expo, on_exception
+from elasticsearch import Elasticsearch
+
+from contextlib import contextmanager
+
+from settings import ELASTIC_PORT, ELASTIC_HOST, elastic_exceptions
+
+
+@contextmanager
+def es_connect(url: str):
+    es = Elasticsearch(url)
+    try:
+        yield es
+    finally:
+        es.close()
+
+
+@on_exception(wait_gen=expo, exception=elastic_exceptions, max_value=10)
+def load_data(data, index):
+    """Функция для загрузки данных в Elasticsearch"""
+    with es_connect(f"http://{ELASTIC_HOST}:{ELASTIC_PORT}/") as es_conn:
+        es_conn.index(index=index, id=data.get("id"), document=data)

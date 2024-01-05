f01fd30cc1e074e19c325465f8645d24bc04df96
evtepo <andrey_laguta00@mail.ru>
2024-01-06 01:49:44 +1000

    Изменил backoff на свой

--- etl/main.py
+++ etl/main.py
@@ -1,8 +1,6 @@
 import psycopg2
 from psycopg2.extensions import cursor as _cursor
 
-from backoff import on_exception, expo
-
 from contextlib import contextmanager
 
 from settings import dsl, pg_exceptions
@@ -10,10 +8,10 @@ from extractor import extract_pg_data
 from transformer import get_valid_data
 from state_redis import get_state
 from loader import load_data
+from etl_backoff import backoff
 
 
 @contextmanager
-@on_exception(wait_gen=expo, exception=pg_exceptions, max_value=10)
 def pg_connect(dsl: dict) -> _cursor:
     db = psycopg2.connect(**dsl)
     try:
@@ -22,6 +20,7 @@ def pg_connect(dsl: dict) -> _cursor:
         db.close()
 
 
+@backoff(exceptions=pg_exceptions)
 def run_movies_etl(dsl: dict):
     with pg_connect(dsl) as pg_conn:
         state = get_state()
@@ -34,11 +33,15 @@ def run_movies_etl(dsl: dict):
         index = "movies"
 
         for items in extract(pg_conn):
+            result = []
             for item in items:
                 item = transform(*item)
                 if state.get_state(item.get("id")) != item:
                     state.set_state(item.get("id"), item)
-                    load(item, index)                   
+                    result.append(item)
+            
+            if result:
+                load(result, index)
 
 
 if __name__ == "__main__":

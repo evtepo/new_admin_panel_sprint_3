c82cb6cfc7c491fe8eb3b73e86e2b7202228c221
evtepo <andrey_laguta00@mail.ru>
2024-01-06 01:51:20 +1000

    Исправил функцию, которая удаляла нужные данные

--- sqlite_to_postgres/delete_data_from_sqlite.py
+++ sqlite_to_postgres/delete_data_from_sqlite.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 
-import sqlite3
+from sqlite3 import Cursor
 
 
 @dataclass(frozen=True)
@@ -8,55 +8,26 @@ class TransfomData:
     """
     Класс для подготовки таблиц к переносу.
     """
-    connection: sqlite3.Connection.cursor
-    tables: tuple = (
-        "film_work", "person_film_work",
-        "genre", "person", "genre_film_work"
-    )
-    names = {
-        "person_film_work": "film_work_id, person_id, role",
-        "genre_film_work": "film_work_id, genre_id",
-        "person": "full_name",
-        "genre": "name",
-        "film_work": "title",
-    }
+    connection: Cursor
+    table: str = "film_work"
 
-    def change_data(self):
-        """
-        Функция для удаления дубликатов.
-        """
-        cursor = self.connection
-
-        for table in self.tables:
-            if table == "film_work":
-                self.delete_wrong_data(cursor, table)
-
-            name = self.names.get(table)
-            cursor.execute(
-                f"""
-                DELETE FROM {table}
-                WHERE ROWID NOT IN (SELECT MIN(ROWID)
-                FROM {table}
-                GROUP BY {name});
-                """
-            )
-
-    def delete_wrong_data(self, cursor, table):
+    def delete_wrong_data(self):
         """
         Функция для удаления лишних колонок.
         """
+        cursor = self.connection
         new_table = "temporary_table"
         cursor.execute(
             f"""
             CREATE TABLE IF NOT EXISTS {new_table} AS
             SELECT id, title, description, creation_date, rating,
             type, created_at, updated_at
-            FROM {table};
+            FROM {self.table};
             """
         )
         cursor.execute(
-            f"DROP TABLE IF EXISTS {table};"
+            f"DROP TABLE IF EXISTS {self.table};"
         )
         cursor.execute(
-            f"ALTER TABLE {new_table} RENAME TO {table};"
+            f"ALTER TABLE {new_table} RENAME TO {self.table};"
         )

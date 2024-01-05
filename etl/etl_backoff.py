a4abf07fafbe94a12b1d38bb6ef2d9bec1920cef
evtepo <andrey_laguta00@mail.ru>
2024-01-06 01:50:38 +1000

    Написал свой backoff

---
+++ etl/etl_backoff.py
@@ -0,0 +1,35 @@
+import logging
+import random
+import time
+
+from functools import wraps
+
+
+def backoff(start_sleep_time=0.1, factor=2, border_sleep_time=10, exceptions=None):
+    """
+    Функция для повторного выполнения функции через некоторое время, если возникла ошибка.
+    Использует наивный экспоненциальный рост времени повтора (factor) до граничного времени ожидания (border_sleep_time)
+    """
+
+    def func_wrapper(func):
+        @wraps(func)
+        def inner(*args, **kwargs):
+            n = 1
+            while True:
+                t = start_sleep_time * (factor**n)
+                try:
+                    time.sleep(
+                        random.uniform(
+                            0.1,
+                            t if t < border_sleep_time else border_sleep_time
+                        )
+                    )
+                    return func(*args, **kwargs)
+                except exceptions:
+                    logging.error(f"An error occurred from this list: {exceptions}")
+                    n += 1
+                    continue
+
+        return inner
+
+    return func_wrapper

e3cfafafb2f8f81755ed193a768fef5ef21280e9
evtepo <andrey_laguta00@mail.ru>
2024-01-06 01:55:31 +1000

    Переместил файлы

--- movies_admin/launch.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-./wait-for-it.sh db:"${DB_PORT}" --timeout=-60
-./wait-for-it.sh elasticsearch:"${ELASTIC_PORT}" --timeout=-60
-
-./index_creation.sh
-./migrations.sh
--- movies_admin/migrations.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-python manage.py collectstatic --noinput
-
-python manage.py migrate
-python manage.py migrate sessions
-python manage.py migrate movies 0001 --fake
-
-uwsgi --strict --ini uwsgi.ini

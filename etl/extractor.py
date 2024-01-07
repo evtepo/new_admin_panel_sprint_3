7a3889acd702588d53821686f1cf103fe3ad4953
evtepo <andrey_laguta00@mail.ru>
2024-01-07 16:38:35 +1000

    Изменил сбор данных из PostgreSQL, добавил сбор данных по связанным моделям

--- etl/extractor.py
+++ etl/extractor.py
@@ -1,14 +1,19 @@
 from psycopg2.extensions import cursor as _cursor
 
 from typing import Union, List, Tuple
-from backoff import on_exception, expo
-
-from settings import pg_exceptions
 
 
-@on_exception(wait_gen=expo, exception=pg_exceptions, max_value=10)
-def extract_pg_data(cursor: _cursor, limit: int = 100) -> List[Union[Tuple, None]]:
+def extract_pg_data(
+    cursor: _cursor, sub_query: str = None, modified: str = "", limit: int = 100
+) -> List[Union[Tuple, None]]:
     """Функция для сбора данных из PostgreSQL"""
+
+    command = ""
+
+    if modified:
+        command = f"""WHERE fw.modified > timestamp '{modified}'"""
+        if sub_query:
+            command += f" OR fw.id IN ({sub_query})"
 
     cursor.execute(
         f"""
@@ -26,24 +31,83 @@ def extract_pg_data(cursor: _cursor, limit: int = 100) -> List[Union[Tuple, None
                         )
                     ) FILTER (WHERE p.id is not null),
                     '[]'
-                ) as persons
+                ) as persons,
+                fw.modified
         FROM content.film_work fw
         LEFT OUTER JOIN content.genre_film_work gfw
             ON (fw.id = gfw.film_work_id)
         LEFT OUTER JOIN content.genre g
             ON (gfw.genre_id = g.id)
-        LEFT OUTER JOIN content.person_film_work pfw
+        LEFT OUTER JOIN content.person_film_work pfw 
             ON (fw.id = pfw.film_work_id)
         LEFT OUTER JOIN content.person p
             ON (pfw.person_id = p.id)
+        {command}
         GROUP BY fw.id
         ORDER BY fw.modified
+        LIMIT {limit}
         """
     )
 
     while True:
-        data = cursor.fetchmany(limit)
+        data = cursor.fetchall()
         if not data:
             break
 
         yield data
+
+
+def get_last_record(cursor: _cursor, table_name: str) -> tuple:
+    cursor.execute(
+        f"""
+        SELECT id, modified
+        FROM content.{table_name}
+        ORDER BY modified DESC
+        LIMIT 1
+        """
+    )
+
+    return cursor.fetchone()
+
+
+def get_sub_record_id(cursor: _cursor, table_name: str, modified: str) -> str:
+    cursor.execute(
+        f"""
+        SELECT id, modified
+        FROM content.{table_name}
+        WHERE modified > timestamp '{modified}'
+        """
+    )
+
+    t_id = cursor.fetchone()
+
+    if t_id:
+        return t_id
+
+
+def get_sub_records(cursor: _cursor, table_name: str, t_id: str) -> list:
+    cursor.execute(
+        f"""
+        SELECT fw.id
+        FROM content.film_work fw
+        LEFT JOIN content.{table_name}_film_work tfw ON tfw.film_work_id = fw.id
+        WHERE tfw.{table_name}_id = '{t_id}'
+        """
+    )
+
+    result = cursor.fetchall()
+
+    if result:
+        return result
+
+
+def get_query(pg_conn: _cursor, table_name: str, data: str, modified_field: str, state):
+    """
+    Функция для получения id film_work,
+    у которых изменилось поле genre или person
+    """
+    state.set_state(modified_field, str(data[-1]))
+    records = get_sub_records(pg_conn, str(table_name), f"{data[0]}")
+    query = ", ".join(map(lambda x: f"'{x[0]}'", records))
+
+    return query

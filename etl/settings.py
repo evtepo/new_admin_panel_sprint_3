a6e1e94cab14194abb25f7ccdc1d8b2a0b502bc9
evtepo <andrey_laguta00@mail.ru>
2024-01-08 16:16:09 +1000

    Изменил настройки для подключения

--- etl/settings.py
+++ etl/settings.py
@@ -1,48 +1,53 @@
-import os
-
 import logging
 
 from dotenv import load_dotenv
-
-from psycopg2 import OperationalError, DatabaseError
-from redis import ConnectionError
 from elasticsearch import ConnectionError
+from psycopg2 import DatabaseError, OperationalError
+from pydantic import Field
+from pydantic_settings import BaseSettings
+from redis import ConnectionError
 
 
 load_dotenv()
 
-ELASTIC_PORT = os.environ.get("ELASTIC_PORT")
-ELASTIC_HOST = os.environ.get("ELASTIC_HOST")
 
-REDIS_HOST = os.environ.get("REDIS_HOST")
-REDIS_PORT = os.environ.get("REDIS_PORT")
+class Settings(BaseSettings):
+    elastic_http: str = Field(alias="ELASTIC_HTTP")
+    elastic_host: str = Field(alias="ELASTIC_HOST")
+    elastic_port: str = Field(alias="ELASTIC_PORT")
 
-DB_NAME = os.environ.get("DB_NAME")
-DB_USER = os.environ.get("DB_USER")
-DB_PASSWORD = os.environ.get("DB_PASSWORD")
-DB_HOST = os.environ.get("DB_HOST")
-DB_PORT = os.environ.get("DB_PORT")
+    redis_host: str = Field(alias="REDIS_HOST")
+    redis_port: str = Field(alias="REDIS_PORT")
 
-dsl = {
-    "dbname": DB_NAME,
-    "user": DB_USER,
-    "password": DB_PASSWORD,
-    "host": DB_HOST,
-    "port": DB_PORT,
+    db_name: str = Field(alias="DB_NAME")
+    db_user: str = Field(alias="DB_USER")
+    db_password: str = Field(alias="DB_PASSWORD")
+    db_host: str = Field(alias="DB_HOST")
+    db_port: str = Field(alias="DB_PORT")
+
+    elastic_exceptions: tuple = (
+        ConnectionError,
+        DatabaseError,
+    )
+
+    redis_exceptions: tuple = (ConnectionError,)
+
+    pg_exceptions: tuple = (OperationalError,)
+
+
+elastic_dsn: str = (
+    f"{Settings().elastic_http}://{Settings().elastic_host}:{Settings().elastic_port}/"
+)
+
+redis_dsn: str = f"redis://{Settings().redis_host}:{Settings().redis_port}"
+
+postgres_dsl: dict = {
+    "dbname": Settings().db_name,
+    "user": Settings().db_user,
+    "password": Settings().db_password,
+    "host": Settings().db_host,
+    "port": Settings().db_port,
 }
-
-elastic_exceptions = (
-    ConnectionError,
-    DatabaseError,
-)
-
-redis_exceptions = (
-    ConnectionError,
-)
-
-pg_exceptions = (
-    OperationalError,
-)
 
 filename = logging.FileHandler("backoff_log.log")
 console = logging.StreamHandler()

377c1ce6ab14640d38145eec944303f36ce66747
evtepo <andrey_laguta00@mail.ru>
2024-01-08 16:14:02 +1000

    Изменил файл с настройками

--- sqlite_to_postgres/settings.py
+++ sqlite_to_postgres/settings.py
@@ -1,14 +1,37 @@
+import logging
 import os
 
 from dotenv import load_dotenv
+from pydantic import Field
+from pydantic_settings import BaseSettings
 
 
 load_dotenv()
 
-DB_NAME = os.environ.get("DB_NAME")
-DB_USER = os.environ.get("DB_USER")
-DB_PASSWORD = os.environ.get("DB_PASSWORD")
-DB_HOST = os.environ.get("DB_HOST")
-DB_PORT = os.environ.get("DB_PORT")
 
-SQLITE = os.environ.get("SQLITE")
+class Settings(BaseSettings):
+    db_name: str = Field(alias="DB_NAME")
+    db_user: str = Field(alias="DB_USER")
+    db_password: str = Field(alias="DB_PASSWORD")
+    db_host: str = Field(alias="DB_HOST")
+    db_port: str = Field(alias="DB_PORT")
+
+    sqlite = os.environ.get("SQLITE")
+
+
+postgres_dsl: dict = {
+    "dbname": Settings().db_name,
+    "user": Settings().db_user,
+    "password": Settings().db_password,
+    "host": Settings().db_host,
+    "port": Settings().db_port,
+}
+
+filename = logging.FileHandler("load_data_log.log")
+console = logging.StreamHandler()
+
+logging.basicConfig(
+    handlers=(filename, console),
+    level=logging.INFO,
+    format="[%(asctime)s] %(levelname)s: %(message)s",
+)

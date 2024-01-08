e31abe48b563a4566910965c5cfac19741dc1cea
evtepo <andrey_laguta00@mail.ru>
2024-01-08 16:18:39 +1000

    Отсортировал импорты/изменил подключение к Elasticsearch

--- etl/loader.py
+++ etl/loader.py
@@ -1,10 +1,9 @@
-from elasticsearch import Elasticsearch
-from elasticsearch.helpers import bulk
-
 from contextlib import contextmanager
 
-from settings import ELASTIC_PORT, ELASTIC_HOST, elastic_exceptions
+from elasticsearch import Elasticsearch
+from elasticsearch.helpers import bulk
 from etl_backoff import backoff
+from settings import Settings, elastic_dsn
 
 
 @contextmanager
@@ -16,9 +15,9 @@ def es_connect(url: str):
         es.close()
 
 
-@backoff(exceptions=elastic_exceptions)
+@backoff(exceptions=Settings.elastic_exceptions)
 def load_data(data, index):
     """Функция для загрузки данных в Elasticsearch"""
-    with es_connect(f"http://{ELASTIC_HOST}:{ELASTIC_PORT}/") as es_conn:
+    with es_connect(elastic_dsn) as es_conn:
         data = [{"_index": index, "_id": item.get("id"), "_source": item} for item in data]
         bulk(client=es_conn, actions=data)
